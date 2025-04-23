1ca0ae56c598fd7b4edc2ec790691b3ed535fc27
rdl-rcd <rdl-rcd@bni.co.id>
2025-04-23 11:23:18 +0700

    Adding Function Create, Update and Delete

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
@@ -16,5 +17,70 @@ class ProductController extends Controller
         return view('pages.products.index', [
             'products' => $products,
         ]);
+    }
+    public function create()
+    {
+        $categories = Category::all();
+        return view('pages.products.create', [
+            'categories' => $categories,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|min:3',
+            'price' => 'required',
+            'stock' => 'required',
+            'description' => 'nullable',
+            'sku' => 'required',
+            'category_id' => 'required',
+        ]);
+
+        Product::create($validated);
+
+        return redirect('/products');
+    }
+
+    public function delete($id)
+    {
+        $product = Product::where('id', $id);
+        $product->delete();
+
+        return redirect('/products');
+    }
+
+    public function edit($id)
+    {
+        $product = Product::findOrFail($id);
+        $categories = Category::all();
+
+        return view('pages.products.edit', [
+            'product' => $product,
+            'categories' => $categories,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'name' => 'required|min:3',
+            'price' => 'required',
+            'stock' => 'required',
+            'description' => 'nullable',
+            'sku' => 'required',
+            'category_id' => 'required',
+        ]);
+
+        Product::where('id', $id)->update([
+            'name' => $request->input('name'),
+            'price' => $request->input('price'),
+            'stock' => $request->input('stock'),
+            'description' => $request->input('description'),
+            'sku' => $request->input('sku'),
+            'category_id' => $request->input('category_id'),
+        ]);
+
+        return redirect('/products');
     }
 }
--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -19,6 +19,11 @@
     <div class="row">
         <div class="col">
             <div class="card">
+                <div class="card-header d-flex justify-content-end">
+                    <a href="/products/create" class="btn btn-sm btn-primary">
+                        Tambah Produk
+                    </a>
+                </div>
                 <div class="card-body">
                     <table class="table table-bordered">
                         <thead>
@@ -30,6 +35,7 @@
                                 <th>Harga</th>
                                 <th>Stok</th>
                                 <th>Kategori</th>
+                                <th>#</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -37,11 +43,21 @@
                                 <tr>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $product->name }}</td>
-                                    <td>{{ $product->description }}</td>
+                                    <td>{{ $product->description ?? '-' }}</td>
                                     <td>{{ $product->sku }}</td>
                                     <td>{{ $product->price }}</td>
                                     <td>{{ $product->stock }}</td>
                                     <td>{{ $product->category->name }}</td>
+                                    <td>
+                                        <div class="d-flex">
+                                            <a href="/products/edit/{{ $product->id }}" class="btn btn-sm btn-warning mr-2">Edit</a>
+                                            <form action="/products/{{ $product->id }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">Hapus</button>
+                                            </form>
+                                        </div>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
